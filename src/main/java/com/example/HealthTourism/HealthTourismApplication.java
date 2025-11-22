801ca9c205e17df0a557e7602a894d998e95c036
tevfikuykun <tevfikuykun33@hotmail.com>
2025-11-23 01:25:49 +0300

    Initial commit

---
+++ src/main/java/com/example/HealthTourism/HealthTourismApplication.java
@@ -0,0 +1,13 @@
+package com.example.HealthTourism;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthTourismApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthTourismApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/HealthTourism/HealthTourismApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HealthTourism;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthTourismApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
